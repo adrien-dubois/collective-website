17de355ad945bbcdb01d068aa5ab2edc26c649df
adrien-dubois <mcsnoos@gmail.com>
2022-03-09 16:00:09 +0100

    [WIP]Skillz part

--- src/components/Collective/Skills.jsx
+++ src/components/Collective/Skills.jsx
@@ -1,10 +1,68 @@
 import React from 'react'
 import { Section } from './Styles/Skills.elements'
+import skills1 from '../../assets/img/collective/skills1.png'
+import skills2 from '../../assets/img/collective/skills2.png'
+import Title from './Title'
 
 function Skills() {
+  const skillsData = [
+    {
+      name: "From Scratch",
+      amount: "95"
+    },
+    {
+      name: "Symfony & PHP",
+      amount: "80"
+    },
+    {
+      name: "React & JS",
+      amount: "70"
+    },
+    {
+      name: "Webdesign",
+      amount: "70"
+    },
+    {
+      name: "Sécurité",
+      amount: "95"
+    },
+  ]
   return (
     <Section id="skills">
-      
+      <Title value="skills"/>
+        <div className="background">
+          <img src={skills1} alt='Background blobs' className='design1'/> 
+          <img src={skills2} alt='Background blobs' className='design2'/> 
+        </div>
+        <div className="skill__title">
+          <p>Nos skills</p>
+          <h2>Les compétences du collectif</h2>  
+        </div>
+        <div className="skills">
+          <div className="skills__bars">
+          {
+            skillsData.map(({name, amount}) => {
+              return(
+                <div className="skills__bars__bar" key={name}>
+                  <div className="container">
+                    <progress value={amount} max="100" />
+                    <span>{name}</span>  
+                  </div>
+                  <h3>{amount}%</h3>
+                </div>
+                
+              );
+            })}  
+          </div>
+          <div className="skills__content">
+            <p className="title">
+              Sit eu anim consectetur dolore amet et nisi id minim id ea non.Aliquip velit ea duis aliqua commodo laborum est incididunt.
+            </p>
+            <div className="description">
+              Duis nostrud duis ex nisi. Ut incididunt nostrud consectetur pariatur in proident mollit sint. Fugiat sint nostrud non do est laborum officia sit. Enim tempor et minim nostrud nostrud amet aliquip. Excepteur sunt quis magna velit cillum reprehenderit tempor velit amet reprehenderit fugiat ea anim. 
+            </div>
+          </div>
+        </div>
     </Section>
   )
 }
--- src/components/Collective/Styles/Skills.elements.js
+++ src/components/Collective/Styles/Skills.elements.js
@@ -1,5 +1,95 @@
 import styled from "styled-components"
+import Wallpaper from "../../../assets/img/collective/wallpaper2.svg"
 
 export const Section = styled.section`
-
+    min-height: 100vh;
+    height: 110vh;
+    background-image: url(${Wallpaper});
+    background-size: cover;
+    min-height: 100vh;
+    background-repeat: no-repeat;
+    background-position: center;
+    position: relative;
+    .background{
+        position: relative;
+        .design1{
+            position: absolute;
+            right: 0;
+            z-index: 1;
+        }
+        .design2{
+            position: absolute;
+            left: 0;
+            top: 20rem;
+            z-index: 1;
+        }
+    }
+    .skill__title{
+        padding: 6rem 10rem;
+        p {
+            text-transform: uppercase;
+            letter-spacing: 0.2rem;
+            color: var(--light-chocolate);
+        }
+        h2{
+            color: var(--dark-chocolate);
+            font-size: 1.8rem;
+        }
+    }
+    .skills{
+        display: flex;
+        padding: 0 20rem;
+        gap: 10rem;
+        &__bars{
+            transform: rotate(-90deg);
+            width: max-content;
+            height: max-content;
+            display: flex;
+            flex-direction: column;
+            gap: 4rem;
+            &__bar{
+                display: flex;
+                flex-direction: row-reverse;
+                gap: 1rem;
+            }
+            .container{
+                display: flex;
+                flex-direction: column;
+                gap: 2.5rem;
+                span{
+                    text-transform: uppercase;
+                    letter-spacing: 0.2rem;
+                    color: var(--light-chocolate);
+                }
+                progress{
+                    width: 30rem;
+                    --webkit-appearance: none;
+                    appearance: none;
+                    &::-webkit-progress-bar{
+                        height: 3rem;
+                        background-color: var(--white-antique);
+                    }
+                    &::-webkit-progress-value{
+                        background-color: var(--almond-dark);
+                    }
+                }
+            }
+            h3{
+                transform: rotate(90deg);
+                color: var(--light-chocolate);
+                font-size: 1.8rem;
+            }
+        }
+        &__content{
+            display: flex;
+            flex-direction: column;
+            gap: 2rem;
+            color: var(--light-chocolate);
+            z-index: 2;
+            .title{
+                font-weight: bolder;
+                letter-spacing: 0.1rem;
+            }
+        }
+    }
 `;
